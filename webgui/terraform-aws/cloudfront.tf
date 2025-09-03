fa9e076c1ecf953c2470161b389ca4e13ae22f5f
coy102 <fajarsep12@gmail.com>
2025-09-03 13:22:44 +0700

    chore: update contet security policy

--- webgui/terraform-aws/cloudfront.tf
+++ webgui/terraform-aws/cloudfront.tf
@@ -14,26 +14,27 @@ resource "aws_cloudfront_response_headers_policy" "bui-security-headers-policy" 
   name = "bui-security-headers-policy"
 
   security_headers_config {
-    content_security_policy {
-      override = true
-      content_security_policy = join(" ", [
-        "default-src 'none';",
-        "script-src 'self' 'sha256-2P8mXF+NOGY5a6oJ1jDjLINrckn9RgJYdEesn+Qf4rQ=' https://cdn.jsdelivr.net/npm/igv@3.1.2/dist/igv.min.js;",
-        "style-src 'self' 'unsafe-inline';",
-        "font-src 'self' https://fonts.gstatic.com;",
-        "img-src 'self' data: content: blob: https://${var.data_portal_bucket}.s3.${var.region}.amazonaws.com;",
-        "form-action 'self';",
+  content_security_policy {
+    override = true
+    content_security_policy = join(" ", [
+      "default-src 'none';",
+      "script-src 'self' 'sha256-2P8mXF+NOGY5a6oJ1jDjLINrckn9RgJYdEesn+Qf4rQ=' https://cdn.jsdelivr.net/npm/igv@3.1.2/dist/igv.min.js;",
+      "style-src 'self' 'unsafe-inline';",
+      "font-src 'self' https://fonts.gstatic.com;",
+      "img-src 'self' data: content: blob: https://${var.data_portal_bucket}.s3.${var.region}.amazonaws.com;",
+      "form-action 'self';",
 
-        "connect-src 'self' ${var.api_endpoint_sbeacon} ${var.api_endpoint_clinic}",
-        "https://cognito-identity.${var.region}.amazonaws.com https://cognito-idp.${var.region}.amazonaws.com",
-        "https://${var.data_portal_bucket}.s3.${var.region}.amazonaws.com https://api.pricing.us-east-1.amazonaws.com",
-        "https://igv.org/genomes/ https://hgdownload.soe.ucsc.edu/goldenPath/hg38/ https://eutils.ncbi.nlm.nih.gov/entrez/eutils/ https://r4.ontoserver.csiro.au https://www.ebi.ac.uk;",
-        "frame-ancestors 'none';",
-        "base-uri 'self';",
-        "object-src 'none';",
-        "manifest-src 'self';",
-      ])
-    }
+      "connect-src 'self' ${var.api_endpoint_sbeacon} ${var.api_endpoint_clinic}",
+      "https://cognito-identity.${var.region}.amazonaws.com https://cognito-idp.${var.region}.amazonaws.com",
+      "https://${var.data_portal_bucket}.s3.${var.region}.amazonaws.com https://api.pricing.us-east-1.amazonaws.com",
+      "https://secretsmanager.${var.region}.amazonaws.com https://kms.${var.region}.amazonaws.com",
+      "https://igv.org/genomes/ https://hgdownload.soe.ucsc.edu/goldenPath/hg38/ https://eutils.ncbi.nlm.nih.gov/entrez/eutils/ https://r4.ontoserver.csiro.au https://www.ebi.ac.uk;",
+      "frame-ancestors 'none';",
+      "base-uri 'self';",
+      "object-src 'none';",
+      "manifest-src 'self';",
+    ])
+  }
 
     content_type_options {
       override = true
